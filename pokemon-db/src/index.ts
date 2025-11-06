4e41e647d13eb11400b407fcd5f5c179c4663253
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2025-11-06 09:48:11 +0100

    added start code

---
+++ pokemon-db/src/index.ts
@@ -0,0 +1,43 @@
+const sectionSetup = () => {
+
+}
+
+const navSetup = () => {
+
+}
+
+const toggleSectionDisplay = () => {
+
+}
+
+const pokedexSetup = () => {
+
+}
+
+const fetchAllPokemons = () => {
+
+}
+
+const fetchPokemonDetails = () => {
+
+}
+
+const renderPokedex = () => {
+
+}
+
+const createCard = () => {
+
+}
+
+const calculateTotal = () => {
+    
+}
+
+const capitalizeWords = (str : string) => {
+    if(str) {
+        return str.replace(/\b\w/g, (match) => match.toUpperCase());
+    } else {
+        return '-';
+    }
+}
